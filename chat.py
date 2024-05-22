2f37ef9485ef84c4b996f09b33f9c5203f64f3d6
aishwaryabhagatt <103053346+aishwaryabhagatt@users.noreply.github.com>
2024-05-22 22:23:34 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,9 +1,11 @@
-import streamlit as st
 import json
 import numpy as np
-import pickle
+import streamlit as st
+import warnings
+warnings.filterwarnings("ignore")
 from tensorflow import keras
 from sklearn.preprocessing import LabelEncoder
+import pickle
 
 # Load the intents file
 with open('intents.json') as file:
@@ -18,35 +20,43 @@ def load_model():
         st.error(f"Error loading model: {e}")
         return None
 
-model = load_model()
+@st.cache_resource
+def load_tokenizer():
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    return tokenizer
 
+@st.cache_resource
+def load_label_encoder():
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+    return lbl_encoder
+
+# Load the model and other files
+model = load_model()
+tokenizer = load_tokenizer()
+lbl_encoder = load_label_encoder()
+
+# Check if the model is loaded successfully
 if model is None:
     st.stop()
 
-# Load tokenizer object
-with open('tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-
-# Load label encoder object
-with open('label_encoder.pickle', 'rb') as enc:
-    lbl_encoder = pickle.load(enc)
-
+# Parameters
 max_len = 20
 
-st.title("Pandora: Your Personal Therapeutic AI Assistant")
-st.write("Start talking with Pandora. Type 'quit' to stop.")
+st.title("Pandora - Your Personal Therapeutic AI Assistant")
 
-user_input = st.text_input("You:", key="user_input")
-
-if user_input:
+def chat():
+    st.write("Start talking with Pandora (type 'quit' to stop).")
+    user_input = st.text_input("You: ", "")
     if user_input.lower() == 'quit':
         st.write("Pandora: Take care. See you soon.")
-    else:
-        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([user_input]), truncating='post', maxlen=max_len))
-        tag = lbl_encoder.inverse_transform([np.argmax(result)])
+    elif user_input:
+        with st.spinner("Pandora is thinking..."):
+            result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([user_input]), truncating='post', maxlen=max_len))
+            tag = lbl_encoder.inverse_transform([np.argmax(result)])
+            for i in data['intents']:
+                if i['tag'] == tag:
+                    st.write("Pandora: " + np.random.choice(i['responses']))
 
-        for i in data['intents']:
-            if i['tag'] == tag:
-                response = np.random.choice(i['responses'])
-                st.write(f"Pandora: {response}")
-                break
+chat()
